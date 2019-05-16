ec7cc040e93172ddf16b1db9d0ed33a580bf1a1a
Robert Melendez <robertm3lendez@gmail.com>
2019-05-16 13:41:40 -0400

    navbar setup

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home: React.SFC = () => {
+  return (
+    <div style={{ marginTop: 20 }} className="container">
+      <h3 className="center">Welcome Home!</h3>
+    </div>
+  );
+};
+
+export default Home;
